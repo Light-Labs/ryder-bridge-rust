54b25ed54365ec4b2d48fd17048f66367a955f89
Friedger Müffke <mail@friedger.de>
2022-05-03 16:22:45 +0200

    add mutex for serial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::HashMap,
     env,
     io::Error as IoError,
     net::SocketAddr,
@@ -7,7 +6,7 @@ use std::{
     time::Duration,
 };
 
-use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_channel::mpsc::unbounded;
 use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 
 use tokio::net::{TcpListener, TcpStream};
@@ -15,18 +14,20 @@ use tungstenite::protocol::Message;
 
 use serialport;
 
-type Tx = UnboundedSender<Message>;
-type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+type DeviceOwner = Arc<Mutex<Option<SocketAddr>>>;
 
-async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
+async fn handle_connection(device_owner: DeviceOwner, raw_stream: TcpStream, addr: SocketAddr) {
+    let previous_owner = *device_owner.lock().unwrap();
+    if previous_owner.is_none() {
+        *device_owner.lock().unwrap() = Some(addr);
+    }
+
     println!("Incoming TCP connection from: {}", addr);
 
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
     println!("WebSocket connection established: {}", addr);
-
-    // Insert the write part of this peer to the peer map.
     let (tx, rx) = unbounded();
     let (outgoing, incoming) = ws_stream.split();
 
@@ -37,23 +38,26 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
         .expect("Failed to open port");
 
     let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!(
-            "Received a message from {}: {}",
-            addr,
-            msg.to_text().unwrap()
-        );
-        let data = msg.into_data();
-        println!("{:?}", data);
-        port.write(&data).expect("Write failed!");
-        let mut response: Vec<u8> = vec![0; 1000];
-        match port.read(response.as_mut_slice()) {
-            Ok(t) => {
-                println!("size: {}", t);
-                tx.unbounded_send(Message::binary(&response[..t])).unwrap();
+        let previous_owner = *device_owner.lock().unwrap();
+        if previous_owner.is_none() || previous_owner.unwrap() == addr {
+            *device_owner.lock().unwrap() = Some(addr);
+
+            let data = msg.into_data();
+            println!("Received a message from {}: {:?}", addr, data);
+            port.write(&data).expect("Write failed!");
+            let mut response: Vec<u8> = vec![0; 1000];
+            match port.read(response.as_mut_slice()) {
+                Ok(t) => {
+                    println!("read {} bytes", t);
+                    tx.unbounded_send(Message::binary(&response[..t])).unwrap();
+                }
+                Err(e) => eprintln!("{:?}", e),
             }
-            Err(e) => eprintln!("{:?}", e),
+            future::ok(())
+        } else {
+            tx.unbounded_send(Message::text("Ryder in use")).unwrap();
+            future::ok(())
         }
-        future::ok(())
     });
 
     let receive_from_others = rx.map(Ok).forward(outgoing);
@@ -62,7 +66,7 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
     future::select(broadcast_incoming, receive_from_others).await;
 
     println!("{} disconnected", &addr);
-    peer_map.lock().unwrap().remove(&addr);
+    *device_owner.lock().unwrap() = None;
 }
 
 #[tokio::main]
@@ -71,13 +75,12 @@ async fn main() -> Result<(), IoError> {
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    let state = PeerMap::new(Mutex::new(HashMap::new()));
+    let state = DeviceOwner::new(Mutex::new(None));
 
     // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
     let listener = try_socket.expect("Failed to bind");
     println!("Listening on: {}", addr);
-
     // Let's spawn the handling of each connection in a separate task.
     while let Ok((stream, addr)) = listener.accept().await {
         tokio::spawn(handle_connection(state.clone(), stream, addr));
